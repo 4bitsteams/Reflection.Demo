f36c57d500896311baf97fd73abe38aec46b258e
md.rubel <md.rubel@HOAP2406101764A>
2021-11-26 00:30:25 +0600

    Add project files.

---
+++ PrintAll/CustomPrint.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrintAll
+{
+    public class CustomPrint
+    {
+        private string name;
+        public void Print()
+        {
+            Console.WriteLine("Printing from Print");
+            PrintKey();
+        }
+       
+        public void PrintKey()
+        {
+            Console.ReadKey();
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+
+        public void PrintName()
+        {
+            Console.WriteLine($"Name Set as {name}");
+        }
+
+        public void Print(string name)
+        {
+            Console.WriteLine($"Name Pass Name:{name}");
+        }
+
+        public void PrintPrivate()
+        {
+            Console.WriteLine("Print From Private");
+        }
+
+        public string Name => name;
+
+        public static string StaticName =>"Static Property Name";
+    }
+}
